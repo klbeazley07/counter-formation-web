6cbd3491f880cf91a69a2ce60e67969ddedb4c94
klbeazley07 <klbeazley@gmail.com>
2026-03-22 21:46:59 -0500

    updates

--- src/SevenDayChallenge.jsx
+++ src/SevenDayChallenge.jsx
@@ -593,6 +593,7 @@ function CornerNav() {
         src="/helmet.png"
         onError={e => { e.target.style.display = "none"; }}
         alt="Counter Formation"
+        style={{ filter: "brightness(0) invert(1)" }}
       />
       <span>Counter Formation</span>
     </Link>
